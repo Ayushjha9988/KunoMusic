1307d88e413593cb6617297a4b76d6b53152378f
Ayush jha <105988266+Ayushjha9988@users.noreply.github.com>
2024-01-12 09:05:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/aashuxxD/KunoMusic",
+    "https://github.com/Ayushjha9988/KunoMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
